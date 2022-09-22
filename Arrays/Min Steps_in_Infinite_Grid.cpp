bd7225a64cbd661980972c387c4353eb972faf9b
Devendra Singh Rana <60600782+thakursachin05@users.noreply.github.com>
2022-09-22 23:17:28 +0530

    Create Min Steps_in_Infinite_Grid.cpp

---
+++ Arrays/Min Steps_in_Infinite_Grid.cpp
@@ -0,0 +1,10 @@
+// https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
+// Min Steps in Infinite Grid
+
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    int totalMoves = 0;
+    for(int i=0;i<A.size()-1;i++){
+        totalMoves += max(abs(A[i]-A[i+1]),abs(B[i]-B[i+1]));
+    }
+    return totalMoves;
+}
